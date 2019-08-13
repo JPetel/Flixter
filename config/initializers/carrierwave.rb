7a595b16b1abd2b02e71dbc0c57993eaaec277b8
Jpetel <Jacob.Petel@outlook.com>
2019-08-12 22:15:19 -0400

    fixing key error

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,8 +4,8 @@ CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws'                        # required
   config.fog_credentials = {
     provider:              'AWS',                        # required
-    aws_access_key_id:     ENV["AKIAWLMPJBB46RXLNW4H"],        # required
-    aws_secret_access_key: ENV["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"],        # required
+    aws_access_key_id:     ENV["AWS_ACCESS_KEY"],        # required
+    aws_secret_access_key: ENV["AWS_SECRET_KEY"],        # required
   }
-  config.fog_directory  = ENV["flixterbucketjp"]              # required
+  config.fog_directory  = ENV["AWS_BUCKET"]              # required
 end
