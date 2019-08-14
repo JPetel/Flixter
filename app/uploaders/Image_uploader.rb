f7518a49baf6e5ecc0e7d922ae384ac2b1091ecb
Jpetel <Jacob.Petel@outlook.com>
2019-08-14 19:39:29 -0400

    error correction

--- app/uploaders/Image_uploader.rb
+++ app/uploaders/Image_uploader.rb
@@ -22,7 +22,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
-  process resize_to_fill: [800, 250]
+  process resize_to_fill: [800, 350]
   # process scale: [200, 300]
   #
   # def scale(width, height)
